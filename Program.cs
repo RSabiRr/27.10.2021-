d463f041f2f4f6fbb9f424510d51fcf833f7f323
RSabiRr <92287092+RSabiRr@users.noreply.github.com>
2021-10-27 01:05:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+         
+        static void Main(string[] args)
+        {
+            #region task1
+            //    Console.WriteLine(cemvehasil(3, 5));
+
+            //    static string cemvehasil(int a, int b)
+            //    {
+            //        string result = $"Cem'in neticesi {a} ve {b} cixirki {a + b} , Hasilin neticesi: {a} ve {b} ise {a * b} ";
+
+            //       return result;
+            #endregion
+
+            #region task2
+            //int sum = 0;
+
+            //Console.WriteLine("1 ci ededi daxil edin: ");
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("2 ci ededi daxil edin: ");
+
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+            //int sum1 = cem(a, b);
+
+            //Console.WriteLine($"{a} dan {b} qeder cemi hesabla: {sum1} ");
+
+
+            //static int cem(int a, int b)
+            //{
+            //    int sum1 = 0;
+
+            //    for (int i = a; i <= b; i++)
+            //    {
+
+
+
+            //        sum1 += i;
+
+
+
+            //    }
+            //    return sum1;
+
+            //}
+            #endregion
+
+            #region task3
+            //Console.WriteLine("1 ci eded:  ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("2 ci eded:  ");
+
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //int sum1 = sum(a, b);
+
+            //Console.WriteLine($"{a}  ededi ile {b}'nin toplanmasi: {sum1}");
+
+            //static int sum(int a, int b)
+            //{
+            //    int sum1;
+            //    sum1 = a + b;
+
+            //    return sum1;
+            //}
+
+            #endregion
+
+            #region task4(1)
+            //int e1 = cavab(1, 1);
+            //int e2 = cavab2(1 ,1);
+            //int e3 = cavab3(1, 1);
+            //int e4 = cavab4(1, 1);
+
+
+            //Console.WriteLine($" {e1} {e2} {e3} {e4}");
+
+
+
+            //static int cavab(int a,int b)
+            //{
+
+
+            //    int e1 = -1 + 4 * 6;
+
+
+
+            //    return e1;
+
+
+            //}
+
+
+            //static int cavab2(int a2, int b2)
+            //{
+            //    int e2 = (35 + 5) % 7;
+
+            //    return e2;
+            //}
+
+            //static int cavab3(int a3, int b3)
+            //{
+            //    int e3 = 14 + (-4) * 6 / 11;
+
+            //    return e3;
+            //}
+
+            //static int cavab4(int a4, int b4)
+            //{
+            //    int e4 = 2 + 15 / 6 * 1 - 7 % 2;
+
+            //    return e4;
+            //}
+            #endregion
+
+
+            #region task4(2)
+            //int a = -1 + 4 * 6;
+            //int b = (35 + 5) % 7;
+            //int c = 14 + (-4) * 6 / 11;
+            //int d = 2 + 15 / 6 * 1 - 7 % 2;
+
+            //Console.WriteLine($"{a} {b} {c} {d}");
+            #endregion
+        }
+
+
+
+
+
+    }
+    }
